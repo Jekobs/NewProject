4fe21e162b8ff42c0c6df58154dc6df43b322d9c
Jek <alexsey_com@mail.ru>
2022-12-30 13:45:15 +0300

    potomu

--- tester.py
+++ tester.py
@@ -1,22 +1,52 @@
+# coding: utf8
+
 # from selenium import webdriver
 
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+# from bs4 import BeautifulSoup as BS
+# import time
+#
+# s = Service('C:\\Users\\Skyne\\PycharmProjects\\NewProject\\chromedriver.exe')
+# driver = webdriver.Chrome(service=s)
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup as BS
 import time
 
-s = Service('C:\\Users\\Skyne\\PycharmProjects\\NewProject\\chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+service = ChromeService(executable_path='C:\\Users\\Skyne\\PycharmProjects\\NewProject\\chromedriver.exe')
+driver = webdriver.Chrome(service=service, options=options)
 
-driver.get("https://www.github.com")
+driver.get("https://nb-bet.com/")
 # driver.implicitly_wait(10)
-time.sleep(5)
-search = driver.find_element(By.ID, 'user_email')
-# 8UrHYfhK3u@8Fxf
-# skynet-bl@gmail.com   alexsey_com@mail.ru Jekobs
-search.send_keys('roman@examle.com')
-search.send_keys(Keys.RETURN)
+time.sleep(3)
+
+search = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/div[1]/div/div[1]/div/div[3]/a[1]")
+search.click()
+time.sleep(3)
+
+search2 = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div/div/div[2]/div/form/div[1]/div[2]/input")
+search2.send_keys('zagubastik@mail.ru')
+search2.send_keys(Keys.RETURN)
+search3 = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div/div/div[2]/div/form/div[2]/div[2]/input")
+search3.send_keys('stik_001')
+search3.send_keys(Keys.RETURN)
+search4 = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div/div/div[2]/div/form/button")
+search4.click()
 
 # driver.implicitly_wait(10)
 time.sleep(10)
+# soup = BS(driver.page_source, 'html.parser')
+with open("progects.html", "w", encoding="utf-8") as file:
+    file.write(driver.page_source)
+# print(driver.page_source)
+driver.close()
